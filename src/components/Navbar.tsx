a592706e4aa26f2424c3b6bae855cf83caa8a15d
v0st0kCode <ivan.thomas@gmail.com>
2025-09-06 04:32:57 +0200

    adjusting mobile top margin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar: React.FC = () => {
       <nav className="container-custom flex justify-center items-center">
         <Link 
           to="/" 
-          className={`font-outfit font-normal text-xl tracking-tight hover:opacity-80 transition-all duration-1000 ease-out text-shadow text-center mt-16 md:mt-24 ${isLoaded ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+          className={`font-outfit font-normal text-xl tracking-tight hover:opacity-80 transition-all duration-1000 ease-out text-shadow text-center mt-10 md:mt-24 ${isLoaded ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
         >
           <div className="text-5xl">iThomas</div>
           <div className="text-sm text-muted-foreground uppercase tracking-wider">DIGITAL PRODUCT DESIGN</div>
